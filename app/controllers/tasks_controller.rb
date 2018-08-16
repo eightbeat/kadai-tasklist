0ef92eb77f146b2aa2a932932f10131746cf9f22
kazuki <sansei.kid011@gmail.com>
2018-08-16 10:52:07 +0000

    lesson12 kadai

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,14 @@
 class TasksController < ApplicationController
+ before_action :require_user_logged_in
+ before_action :correct_user, only: [:destroy]
 
  def index
    @tasks = Task.all
+  if logged_in?
+   @user = current_user
+   @task = current_user.tasks.build  # form_for 用
+   @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
+  end
  end
  
  def show
@@ -13,15 +20,16 @@ class TasksController < ApplicationController
  end
  
  def create
-  @task= Task.new(task_params)
-  
-  if @task.save
-   flash[:success] ="Task が送信されました"
-   redirect_to @task
-  else
-   flash.now[:danger] ="Task が送信されませんでした"
-   render :new
-  end
+ 
+   @task = current_user.tasks.build (task_params)
+   if @task.save
+      flash[:success] = "Task が送信されました"
+      redirect_to root_url
+    else
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
+      flash.now[:danger] = '"Task が送信されませんでした"'
+      render 'new'
+   end
  end
  
  def edit
@@ -40,16 +48,21 @@ class TasksController < ApplicationController
  end
  
  def destroy
-  @task = Task.find(params[:id])
   @task.destroy
-  
-  flash[:success] = "Taskは削除されました"
-  redirect_to tasks_url
+    flash[:success] = 'メッセージを削除しました。'
+    redirect_back(fallback_location: root_path)
  end
  
  private
  
  def task_params
   params.require(:task).permit(:content, :status)
+ end
+ 
+ def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
  end
 end
